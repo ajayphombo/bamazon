285ff545c72e3fa4eb3b1896caba96bd58a946c3
ajayphombo <sagarsu@live.com>
2019-03-27 23:46:51 -0700

    added total counter

--- bamazonCustome.js
+++ bamazonCustome.js
@@ -1,11 +1,10 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var keys = require("./keys");
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
+  // Your port
   port: 3306,
 
   // Your username
@@ -17,23 +16,23 @@ var connection = mysql.createConnection({
 });
 
 
+var total=0; //this will be used to display total price of chosen items
 
 
-// connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
   console.log("connected as id " + connection.threadId);
   readProducts();
 });
-function readProducts() {
-    console.log("Selecting all products...\n");
+function readProducts() {//welcome message and displays the products after retriving data from "products"
+    console.log("Welcome to Bamazon!\n");
     connection.query("SELECT item_id,product_name,price,stock_quantity FROM products  ", function(err, res) {
       if (err) throw err;
       for(var i=0; i<res.length;i++){
-        console.log("Item Id: "+res[i].item_id
+        console.log( 
+                      "Item Id: "+res[i].item_id
                       +"|| Item: " +res[i].product_name
-                      +"|| Price: "+res[i].price
+                      +"|| Price:$ "+res[i].price
                       +"||In Stock: "+res[i].stock_quantity
                     );
       }
@@ -41,12 +40,11 @@ function readProducts() {
     });
   }
 
-  
 function start() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
     inquirer
-      .prompt([
+      .prompt([//prompts and storing user input values
           {
             type: "input",
             message: "\nEnter the ID of the item you want to purchase.",
@@ -60,13 +58,14 @@ function start() {
       
       ])
       .then(function(answer) {
-        for (var i = 0; i < res.length; i++) {
+        for (var i = 0; i < res.length; i++) {//validates the product and stores the total
 
             chosenItem = res[i].product_name;
-            if (res[i].item_id == answer.itemId && res[i].stock_quantity>=answer.amount) {
-              chosenA= answer.amount;
+            if (res[i].item_id === parseInt(answer.itemId) && res[i].stock_quantity>=parseInt(answer.amount)) {
+              var subTotal= parseInt(answer.amount)*res[i].price;
+              total= total+subTotal;
               var newQuantity= res[i].stock_quantity - parseInt(answer.amount);
-              console.log("You want to buy " + parseFloat(answer.amount)+" "+ chosenItem +" .Item Successfully placed in the cart");
+              console.log("\nYou want to buy " + parseFloat(answer.amount)+" "+ chosenItem +" .Item Successfully placed in the cart. Your total so far is :$"+total);
               connection.query("UPDATE products SET ? WHERE ?",
                             [
                                         {
@@ -81,10 +80,17 @@ function start() {
                                           }
                             )
             }
-                else if(res[i].item_id == answer.itemId && res[i].stock_quantity<answer.amount){
-                console.log("Sorry we don't have enough "+chosenItem+". There are only "+res[i].stock_quantity+" available.");
+                else if(res[i].item_id === parseInt(answer.itemId) && res[i].stock_quantity<parseInt(answer.amount)){
+                console.log("\nSorry we don't have enough "+chosenItem+". There are only "+res[i].stock_quantity+" available.");
                 start(); 
            }
+           
+           /*else if(res[i].item_id !== parseInt(answer.itemId)){
+             console.log("\nThe item does not exist! Please Enter a valid ID");
+             readProducts();
+             break;
+
+           }*///here i will try to put default case where if the ID does not exist it will print a message
 
           }
       });
